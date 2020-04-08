67e995c2a38969106679dda4bfb9a69159baf273
Seyi Pythonian <prontomaster@gmail.com>
2020-04-08 18:36:52 +0100

    Modified settings and travis 1

--- config/settings.py
+++ config/settings.py
@@ -3,11 +3,11 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-SERVER_NAME = os.getenv('SERVER_NAME',
-                        'localhost.localdomain:8000')
+# SERVER_NAME = os.getenv('SERVER_NAME',
+#                         'localhost.localdomain:8000')
 
 # Flask-Mail.
-MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'no-reply@example.com')
 MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
 MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
 MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower()
