6c233a6d4312f64db3e66cedad89c6c4ef363f83
Seyi Pythonian <prontomaster@gmail.com>
2020-04-08 18:16:45 +0100

    Update to settings

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,17 @@
 import os
-from distutils.util import strtobool
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
-# SERVER_NAME = os.getenv('SERVER_NAME',
-#                         'localhost.localdomain:8000')
+SERVER_NAME = os.getenv('SERVER_NAME',
+                        'localhost.localdomain:8000')
 
 # Flask-Mail.
 MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
-MAIL_SERVER = os.getenv('MAIL_SERVER')
-MAIL_PORT = os.getenv('MAIL_PORT')
-MAIL_USE_TLS = bool(strtobool(os.getenv('MAIL_USE_TLS')))
-MAIL_USE_SSL = bool(strtobool(os.getenv('MAIL_USE_SSL')))
+MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower()
+MAIL_USE_SSL = os.getenv('MAIL_USE_SSL', 'false').lower()
 MAIL_USERNAME = os.getenv('MAIL_USERNAME')
 MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
